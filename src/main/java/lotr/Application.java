cd0a8ee567c91ca3df8f50bfc3199214fed4442d
curtisnoel <curtis.noel@gmail.com>
2017-11-26 22:13:22 -0500

    added dependency to enable repoistory rest for swagger

--- src/main/java/lotr/Application.java
+++ src/main/java/lotr/Application.java
@@ -2,7 +2,10 @@ package lotr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
+@Import(SpringDataRestConfiguration.class)
 @SpringBootApplication
 public class Application {
 
